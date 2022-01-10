86136d71a8624159aca70244534aaf6b6b3b1de4
Nathan Pham <nathanpham.me@gmail.com>
2022-01-10 09:12:04 -0800

    frontend ui for uploading hand drawn images

--- server.py
+++ server.py
@@ -1,8 +1,28 @@
-from flask import Flask
-app = Flask("app", static_folder="web/static", template_folder="web/templates")
+from flask import Flask, request, render_template
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import base64
+import joblib
+
+# create flask app
+app = Flask("app", static_url_path='/', static_folder="web/static", template_folder="web/templates")
+
+# load model
+label_encoder = joblib.load("model/shapes_label_encoder.joblib")
+classifier = joblib.load("model/shapes_classifier.joblib")
 
 @app.route('/')
 def hello_world():
-  return '<h1>Hello, World!</h1>'
+  return render_template("index.html")
 
-app.run(host="localhost", port=8080)
+@app.route("/api/upload", methods=["POST"])
+def upload():
+    body = request.get_json()
+    pil_image = Image.open(BytesIO(base64.b64decode(body["image"]))).convert('1').resize((200, 200))
+    test_image = [ 255 if pixel else 0 for pixel in np.asarray(pil_image).flatten() ]
+    # save test image to disk
+    pil_image.save("test.png")
+    prediction = label_encoder.inverse_transform(classifier.predict([ test_image ]))
+    return { "prediction": prediction[0] }
+app.run(host="localhost", port=5000, debug=True)
